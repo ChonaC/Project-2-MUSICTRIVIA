1d4a47b84ae8ff2b21ccaa4dfa96e91b5857c880
Daliya Benny <daliyaebenny@gmail.com>
2021-11-08 12:17:51 -0800

    #4

---
+++ seeds/seed.js
@@ -0,0 +1,16 @@
+const sequelize = require('../config/connection');
+const User = require('../models/user');
+const userData = require('./userData.json');
+
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  process.exit(0);
+};
+
+seedDatabase();
