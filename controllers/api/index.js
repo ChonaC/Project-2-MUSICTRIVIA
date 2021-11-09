8de48f7373bb6be40a41d2b84c8ae434ba886179
Jimmy Zheng <jimmyz0622@gmail.com>
2021-11-08 19:46:50 -0800

    Update filenames

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,8 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const songRoutes = require('./song-routes');
+const router = require("express").Router();
+const userRoutes = require("./user-routes");
+const songRoutes = require("./song-routes");
 
-
-router.use('/songs', songRoutes);
-router.use('/users', userRoutes);
+router.use("/songs", songRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
