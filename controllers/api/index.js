bae935639e600d3c6f29fcf5a3b8df63682d2af1
Jimmy Zheng <jimmyz0622@gmail.com>
2021-11-08 20:17:51 -0800

    Add api routes for score

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./user-routes");
 const songRoutes = require("./song-routes");
+const scoreRoutes = require("./score-routes");
 
 router.use("/songs", songRoutes);
 router.use("/users", userRoutes);
+router.use("/scores", scoreRoutes);
 
 module.exports = router;
