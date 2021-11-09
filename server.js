5ba20e9bce400a93ec3a76d7d478055504cfd0ba
Jimmy Zheng <jimmyz0622@gmail.com>
2021-11-08 20:56:09 -0800

    Add script for watch use nodemon

--- server.js
+++ server.js
@@ -1,36 +1,37 @@
-const express = require('express');
-const session = require('express-session');
-const routes = require('./controllers');
-const path = require('path');
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const exphbs = require('express-handlebars');
-
+const express = require("express");
+const session = require("express-session");
+const routes = require("./controllers");
+const path = require("path");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const exphbs = require("express-handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create();
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
+    secret: "Super secret secret",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
 };
 
 app.use(session(sess));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`));
+    app.listen(PORT, () =>
+        console.log(`Server listening at http://localhost:${PORT}`)
+    );
 });
