344b3d13871c103b179cb1e39429103926961814
Jimmy Zheng <jimmyz0622@gmail.com>
2021-11-08 20:10:59 -0800

    Add score model for users score

--- models/Score.js
+++ models/Score.js
@@ -1,25 +1,25 @@
 const { Model, DataTypes } = require("sequelize");
-
 const sequelize = require("../config/connection");
 
-class Song extends Model {}
+class Score extends Model {}
 
-Song.init(
+Score.init(
     {
-        // define columns
         id: {
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
             autoIncrement: true,
         },
-        song_name: {
-            type: DataTypes.STRING,
+        points: {
+            // * Score out of 100% so 1.00 is 100%
+            type: DataTypes.DECIMAL(10, 2),
             allowNull: false,
         },
-        song_url: {
-            type: DataTypes.STRING,
+        date_created: {
+            type: DataTypes.DATE,
             allowNull: false,
+            defaultValue: DataTypes.NOW,
         },
         user_id: {
             type: DataTypes.INTEGER,
@@ -34,8 +34,8 @@ Song.init(
         timestamps: false,
         freezeTableName: true,
         underscored: true,
-        modelName: "Song",
+        modelName: "score",
     }
 );
 
-module.exports = Song;
+module.exports = Score;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require("./User");
 const Song = require("./Song");
+const Score = require("./Score");
 
 User.hasMany(Song, {
     foreignKey: "user_id",
@@ -10,7 +11,17 @@ Song.belongsTo(User, {
     foreignKey: "user_id",
 });
 
+User.hasMany(Score, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE",
+});
+
+Score.belongsTo(User, {
+    foreignKey: "user_id",
+});
+
 module.exports = {
     Song,
     User,
+    Score,
 };
