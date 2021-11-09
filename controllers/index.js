527412fbd0a070cd8b037dea346c23ecc49d2909
Jimmy Zheng <jimmyz0622@gmail.com>
2021-11-08 19:42:01 -0800

    Change routes to lowercase

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+const router = require("express").Router();
+const apiRoutes = require("./api");
+const homeRoutes = require("./homeRoutes");
 
-router.use('/', homeRoutes);
-router.use('/MusicTriviaApi', apiRoutes);
+router.use("/", homeRoutes);
+router.use("/music-trivia-api", apiRoutes);
 router.use((req, res) => {
-    res.send("<h1>Wrong Route!</h1>")
-  });
+    res.send("<h1>Wrong Route!</h1>");
+});
 
 module.exports = router;
