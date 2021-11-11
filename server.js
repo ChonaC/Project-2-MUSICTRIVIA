285866102604c0366bf7aaffedf5c329beeb8dea
Jimmy Zheng <jimmyz0622@gmail.com>
2021-11-10 19:54:09 -0800

    Add helper file for user rank on leaderboard

--- server.js
+++ server.js
@@ -2,19 +2,18 @@ const express = require("express");
 const session = require("express-session");
 const routes = require("./controllers");
 const path = require("path");
-var cors = require('cors')
+var cors = require("cors");
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const exphbs = require("express-handlebars");
-
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 // Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create();
+const hbs = exphbs.create({ helpers });
 const sess = {
-
-    secret: process.env.JAWSDB_SESSION_SECRET,
+    secret: "Super secret secret", // ! changed it from process.env.JAWSDB_SESSION_SECRET to work locally
     cookie: {},
     resave: false,
     saveUninitialized: true,
@@ -26,7 +25,7 @@ const sess = {
 app.use(session(sess));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 // Inform Express.js on which template engine to use
 app.engine("handlebars", hbs.engine);
---
+++ utils/helpers.js
@@ -0,0 +1,5 @@
+module.exports = {
+    get_rank: (index) => {
+        return index + 1;
+    },
+};
